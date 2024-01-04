b88fae41f72a411cf0fe393ffbedffd4ba14d15a
TinoBest <sayayinlenin@hotmail.com>
2024-01-04 17:58:38 -0500

    -apendiente

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,31 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        String C;
+        String T;
+        String R;
+        int X;
+
+        System.out.println("Ingrese 1 para obtener AreaCuadrado");
+        C = entreada nextLine();
+        System.out.println("Ingrese 2 para obtener AreaTriangulo");
+        T = entreada nextLine();
+        System.out.println("Ingrese 3 para obtener AreaRectangulo");
+        R = entreada nextLine();
+        if (x == 1) {
+            C;
+        } else {
+            (x == 2)
+            {
+                T;
+            }else {
+                        (x == 3)
+                    }else {
+                            System.out.println("Dato no valido");
+                            }
+        }
+        
     }
-    
+
 }
