4793eac21840f654bde89b14be299bbc598962f2
tinobest1 <147663974+tinobest1@users.noreply.github.com>
2024-01-10 14:59:50 -0500

    Update Problema7.java

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -11,4 +11,31 @@ package problemas;
  */
 public class Problema7 {
     
+ Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escriba el numero de elementos a ingresar: ");
+        int numElementos = entrada.nextInt();
+        entrada.nextLine();
+
+        String[] ciudades = new String[numElementos];
+        for (int i = 0; i < numElementos; i++) {
+            System.out.print("Ingrese el nombre de la ciudad:\n");
+            ciudades[i] = entrada.nextLine();
+        }
+        return ciudades;
+    }
+
+    public static void obtenerCaracteres(String[] ciudades) {
+        String cadena = "";
+        System.out.printf("Las ciudades con 4 y 5 caracteres son:\n");
+
+        for (int i = 0; i < ciudades.length; i++) {
+            cadena = ciudades[i];
+            if (cadena.length() == 4 || cadena.length() == 5) {
+                System.out.printf("%s\n", cadena);
+            }
+
+        }
+
+    }
 }
