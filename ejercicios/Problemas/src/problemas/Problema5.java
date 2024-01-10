c7f2fff412a1fffdf01fe29ee0d900eb84560f6f
tinobest1 <147663974+tinobest1@users.noreply.github.com>
2024-01-10 14:59:05 -0500

    Update Problema5.java

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -11,4 +11,21 @@ package problemas;
  */
 public class Problema5 {
     
+int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int suma;
+        suma = obtenerSuma(informacion);
+
+        System.out.printf("El resultado de la suma es: %s\n",suma);
+
+    }
+
+    public static int obtenerSuma(int[][] arreglo) {
+        int suma = 0;
+        for (int f = 0; f < arreglo.length; f++) {
+            for (int c = 0; c < arreglo.length; c++) {
+                suma = suma + arreglo [f][c];
+            }
+        }
+        return suma;
+    }
 }
